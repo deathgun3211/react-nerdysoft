7d52dcd8d0094422abb26b7d6cf3687ac91bd853
Meliodas <romamur65@gmail.com>
2020-03-27 03:01:58 +0200

    Updates

---
+++ precache-manifest.bc7e288727f5df5a65d7844cab1c9f26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89153b07b5756a14b17e8064ffd1499a",
+    "url": "/react-nerdysoft/index.html"
+  },
+  {
+    "revision": "81539b0189f8318d59a9",
+    "url": "/react-nerdysoft/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "44401a439c56b5110b46",
+    "url": "/react-nerdysoft/static/css/main.e55fb5af.chunk.css"
+  },
+  {
+    "revision": "81539b0189f8318d59a9",
+    "url": "/react-nerdysoft/static/js/2.c44de53d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-nerdysoft/static/js/2.c44de53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44401a439c56b5110b46",
+    "url": "/react-nerdysoft/static/js/main.6201b6cd.chunk.js"
+  },
+  {
+    "revision": "41b6ec7f77d417c56908",
+    "url": "/react-nerdysoft/static/js/runtime-main.c4a0f530.js"
+  },
+  {
+    "revision": "1c25a58c3ba06359ccd8909dcb05d60a",
+    "url": "/react-nerdysoft/static/media/cat.1c25a58c.jpg"
+  }
+]);
